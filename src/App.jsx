681d74e69dc797f7f7a3ea13f82bec0dfeab6dad
tccsmartlock <tccsmartlock@gmail.com>
2025-09-05 23:35:07 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./Pages/Home"
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../Components/Header"
+
+export function Home() {
+    return(
+        <div>
+            <Header />
+            <h1>home</h1>
+        </div>
+    )
+}
