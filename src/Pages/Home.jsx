dddea786b3ce8bb38fa2b74d65fa2ef2b0937b7d
tccsmartlock <tccsmartlock@gmail.com>
2025-09-09 23:04:07 +0000

    teste legal

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
             <Header />
             <div className="lg:bg-[url('/src/assets/bgHome.png')] bg-[url('/src/assets/bgHomeMobile.png')] min-h-screen bg-no-repeat bg-cover flex flex-col lg:justify-center font-pt-sans lg:gap-10 lg:px-20 lg:pt-50 pt-30 px-5 gap-3">
                 <h1 className="lg:text-6xl text-3xl text-white-text"><span className="lg:text-dark-blue text-header font-bold">Impulsione</span> sua residência de <br /> forma inteligente e segura</h1>
-                <p className="text-white-text text-lg">Com o Guardian Lock, você pode viajar tranquilo, trabalhar focado e <br /> viver com mais liberdade. Se algo acontecer, você será o primeiro a <br /> saber, pois sua segurança é nossa prioridade.</p>
+                <p className="text-white-text ">Com o Guardian Lock, você pode viajar tranquilo, trabalhar focado e <br /> viver com mais liberdade. Se algo acontecer, você será o primeiro a <br /> saber, pois sua segurança é nossa prioridade.</p>
             </div>
         </div>
     )
