f68c38ddf886e6657f393c2320fc17a50acfb8a7
tccsmartlock <tccsmartlock@gmail.com>
2025-09-10 02:41:09 +0000

    detalhes finais, ACABOU PEGA MÁRCIO

--- src/Pages/Jogo.jsx
+++ src/Pages/Jogo.jsx
@@ -10,7 +10,7 @@ export function Jogo() {
             <Header />
             <div className="bg-[url('/src/assets/bgJogoClaro.png')] dark:bg-[url('/src/assets/bgJogoEscuro.png')] bg-cover min-h-screen flex lg:flex-row flex-col justify-center lg:gap-20 gap-5 items-center lg:px-20 px-5 pt-30 pb-5">
                 <div className="flex flex-col lg:gap-20 gap-5 dark:text-white-text font-pt-sans">
-                    <h1 className="lg:block hidden text-7xl text-header-escuro dark:text-white-text">Jogo Guardian Lock <br /> Aventurando</h1>
+                    <h1 className="lg:block hidden text-6xl text-header-escuro dark:text-white-text">Jogo Guardian Lock <br /> Aventurando</h1>
                     <h1 className="lg:hidden text-3xl text-header-escuro dark:text-white-text">Jogo Guardian Lock Aventurando</h1>
                     <p className="lg:block hidden text-2xl">
                     Em um reino esquecido, um castelo se <br /> ergue como o último vestígio de uma <br /> civilização perdida, abrigando mistérios e <br /> forças sobrenaturais. Dentro de suas <br /> paredes, quatro guerreiros marcados pelo <br /> destino enfrentam suas próprias histórias <br /> e lutas pessoais.</p>
@@ -21,9 +21,9 @@ export function Jogo() {
                 <div className="flex flex-col items-center gap-5 lg:text-center dark:text-white-text font-pt-sans">
                     <p className="lg:text-xl text-lg">Para mais contúdo, veja mais aqui:</p>
                     <a href=""><img className="cursor-pointer w-130" src={videoJogo} alt="" /></a>
-                    <button className="lg:block hidden cursor-pointer lg:dark:hidden"><img src={botaoJogoClaro} alt="" /></button>
-                    <button className="lg:hidden block cursor-pointer dark:hidden block"><img src={botaoMobileClaro} alt="" /></button>
-                    <button className="cursor-pointer dark:block hidden"><img src={botaoJogoEscuro} alt="" /></button>
+                    <a href="" className="lg:block hidden cursor-pointer lg:dark:hidden"><img src={botaoJogoClaro} alt="" /></a>
+                    <a href="" className="lg:hidden block cursor-pointer dark:hidden block"><img src={botaoMobileClaro} alt="" /></a>
+                    <a href="" className="cursor-pointer dark:block hidden"><img src={botaoJogoEscuro} alt="" /></a>
                 </div>
             </div>
         </div>
